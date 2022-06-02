0d3cf42f96c66a7bff2990c9f55c971dff5829fa
Deri-Kurniawan <deri.netuchi@gmail.com>
2022-06-03 00:28:05 +0700

    add conditional rendering

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import NoteItem from './NoteItem';
 
 const NotesList = ({ notes, onDelete }) => {
+  if(notes.length === 0) {
+    return <h2>Tidak ada Catatan</h2>
+  }
+
   return (
     <ol>
       {
