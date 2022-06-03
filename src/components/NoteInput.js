150d57623c28cdd6ca4aeeff62d645205c09618b
Deri-Kurniawan <deri.netuchi@gmail.com>
2022-06-03 22:17:50 +0700

    renamed input change func name

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -9,7 +9,7 @@ class NotesInput extends Component {
     }
   }
 
-  handleChange = ({ target }) => {
+  onInputChangeHandler = ({ target }) => {
     this.setState({
       [target.name]: target.value
     })
@@ -35,10 +35,10 @@ class NotesInput extends Component {
       {this.state.body}
         <form onSubmit={this.onSubmitHandler}>
           <label htmlFor='title'></label>
-          <input type='text' name='title' id='title' value={this.state.title} onChange={this.handleChange} placeholder='Title' />
+          <input type='text' name='title' id='title' value={this.state.title} onChange={this.onInputChangeHandler} placeholder='Title' />
           <br/>
           <label htmlFor='body'></label>
-          <textarea name='body' id='body' value={this.state.body} onChange={this.handleChange} cols='30' rows='10' placeholder='Description'></textarea>
+          <textarea name='body' id='body' value={this.state.body} onChange={this.onInputChangeHandler} cols='30' rows='10' placeholder='Description'></textarea>
           <br/>
           <button type='submit'>Submit</button>
         </form>
