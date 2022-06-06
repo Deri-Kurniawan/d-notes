418b0ac60c0ee9874cb3b656a78338cc53656b40
Deri Kurniawan <deri.netuchi@gmail.com>
2022-06-07 00:45:30 +0700

    add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,15 @@ const Footer = () => {
     <div className="py-3 mt-3 bg-light text-center">
       <footer>
         <span>
-          &copy; {new Date().getFullYear()} D-Notes All right reserved.
+          &copy; {new Date().getFullYear()}{" "}
+          <a
+            href="https://github.com/deri-kurniawan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            D-Notes
+          </a>{" "}
+          All right reserved.
         </span>
       </footer>
     </div>
